a838818c727815f0b7041c2bdcaf6e992409abd7
Jouse then <jthen93@gmail.com>
2021-02-28 02:26:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,8 @@ class Song
   
   def self.new_by_filename(filename)
     song = filename.split(" - ")
-    new_song = Song.new(song[1])
-    @@all << new_song
-    new_song.artist = Artist.find_or_create_by_name(song[0])
+    artist = Artist.find_or_create_by_name(song[0])
+    artist.add_song
     
     return new_song
   end
